168a7a65724ef18dcfe0940d7ba42d8af3e0bfa1
ferps <felipebrostolinribeiro@gmail.com>
2025-06-12 23:11:40 -0300

    pointers2

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -12,9 +12,12 @@ const NumError = error{IllegalNumber};
 pub fn main() void {
     const stdout = std.io.getStdOut().writer();
 
-    const my_num: u32 = getNumber();
-
-    try stdout.print("my_num={}\n", .{my_num});
+    const my_num = getNumber();
+    if (my_num) |value| {
+        stdout.print("my_num={d}\n", .{value}) catch return;
+    } else |_| {
+        stdout.print("Error!", .{}) catch return;
+    }
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
